de20754745cb48a82b19d3cc18abf06cf11d7757
tomas <Homero_77>
2021-10-13 18:54:02 -0300

    entero

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,119 @@
+<template>
+  <q-layout view="hHh LpR fff" class="bg-black">
+    <div>
+
+      <q-header class="navbar">
+        <q-toolbar>
+          <a class="logo" href="#">Portfolio.</a>
+          <q-space/>
+          <!-- botones -->
+            <ul>
+              <li><q-btn class="bottom" flat label="Portfolio" v-on:click="handleScroll('portafolio')"/></li>
+              <li><q-btn class="bottom" flat label="Contacto"   v-on:click="handleScroll('contacto')"/> </li>
+              <li>
+
+                <q-btn class="logoIg" flat rounded onclick="location.href='https://www.instagram.com/'">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                </svg>
+              </q-btn>
+
+              </li>
+            </ul>
+        </q-toolbar>
+      </q-header>
+
+      <!-- Video de fondo -->
+      <div height="100px" >
+        <video width="100%"   autoplay muted loop>
+           <source src="https://firebasestorage.googleapis.com/v0/b/tbf2-6b4d5.appspot.com/o/fondo.mp4?alt=media&token=fc9b1eff-744a-488f-894d-efe1b485a13e">
+        </video>
+      </div>
+      <q-page-container class="q-pa-none">
+        <router-view />
+      </q-page-container>
+    </div>
+
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      dialog: true,
+      maximizedToggle: true
+    }
+  },
+  methods: {
+    handleScroll: function (elementName) {
+      document.getElementById(elementName).scrollIntoView({
+        behavior: 'smooth'
+      })
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header')
+  const windowPosition = window.scrollY > 50
+  header.classList.toggle('scrolling-active', windowPosition)
+})
+
+</script>
+
+<style lang="sass" scoped>
+
+.navbar
+  padding: 30px 30px
+  background-color: #ffffff00
+  box-shadow: 0 1px 1rem rgba(255,255,255,.1)
+  transition: background-color .5s ease, padding .5s ease
+
+.scrolling-active
+  padding: 5px 30px
+  background-color: white
+  transition: background-color .5s ease, padding .5s ease
+  transform: 2s
+
+.scrolling-active ul li .bottom
+  color: rgba(0,0,0,.94)
+
+.scrolling-active ul li .bottom:hover
+  border-bottom: 1px solid black
+
+.scrolling-active .logo
+  color: rgba(0,0,0,.94)
+  transform: rotateX(360deg)
+
+ul li
+  display: inline-block
+
+ul li .bottom
+  color: rgba(255,255,255,.94)
+  border-bottom: 1px solid rgba(0, 0, 0 ,0)
+  transition: border-bottom 0.3s
+
+ul li .bottom:hover
+  border-bottom: 1px solid white
+
+.scrolling-active .logoIg
+  color: black
+
+@import url('https://fonts.googleapis.com/css?family=Josefin+Sans')
+
+.logo
+  color: white
+  text-decoration: inherit
+  font-family: 'Josefin Sans'
+  font-size: 35px
+  font-weight: 900
+  transition-duration: .5s
+  transition-property: transform
+
+.body
+  background-color: black
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,209 @@
+<template>
+  <q-page id="portafolio" class="q-pa-none q-ma-none">
+    <!-- Rows para fotos e info -->
+    <!-- Primer fila -->
+    <div  class="row text-center" >
+      <div  class="col-12 col-md-1"></div>
+      <div class="col-12 col-md-5 q-pa-lg text-left">
+        <div class="">
+          <p class="bg-black q-ma-none text-white uppercase"> <strong class="text-green">LOREM</strong> IPSUM IS SIMPLY DUMMY TEXT <strong class="text-green"> THE PRINTING AND TYPESETTING INDUSTRY </strong>. LOREM IPSUM HAS BEEN THE INDUSTRY'S STANDARD DUMMY <strong class="text-green">TEXT EVER SINCE THE 1500</strong> </p>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-5 q-pa-lg">
+        <div class=" img-hover-zoom" style="height:100%" @click="()=>{medium = true; link = 'https://fondosmil.com/fondo/2258.jpg'}">
+          <q-img width="100%" height="100%" class="rounded-borders img" src="https://fondosmil.com/fondo/2258.jpg" />
+        </div>
+      </div>
+
+      <div class="col-12 col-md-1"></div>
+
+    </div>
+    <!-- Segunda fila -->
+    <div  class="row text-center" >
+
+      <div class="col-12 col-md-1"></div>
+
+      <div class="col-12 col-md-5 q-pa-lg">
+        <div class="videoContainer img-hover-zoom" style="height:100%" @click="()=>{mediumVideo = true; linkVideo = 'https://www.youtube.com/embed/xcJtL7QggTI'}">
+          <q-img width="100%" height="100%" class="rounded-borders img" src="https://img.youtube.com/vi/xcJtL7QggTI/maxresdefault.jpg" />
+          <img alt="play" class="playBtn" src="https://i.pinimg.com/originals/a1/1b/ab/a11babe56eada90b29709f82bbf7dec4.png" >
+        </div>
+      </div>
+
+      <div class="col-12 col-md-5 q-pa-lg">
+        <div class="videoContainer img-hover-zoom" style="height:100%" @click="()=>{mediumVideo = true; linkVideo = 'https://www.youtube.com/embed/IBfQXYe0-Lc'}">
+          <q-img width="100%" height="100%" class="rounded-borders img" src="https://img.youtube.com/vi/IBfQXYe0-Lc/maxresdefault.jpg" />
+          <img alt="play" class="playBtn" src="https://i.pinimg.com/originals/a1/1b/ab/a11babe56eada90b29709f82bbf7dec4.png" >
+        </div>
+      </div>
+
+      <div class="col-12 col-md-1"></div>
+
+    </div>
+    <!-- Tercer fila -->
+    <div  class="row text-center" >
+      <div class="col-12 col-md-1"></div>
+
+      <div class="col-12 col-md-5 q-pa-lg">
+        <div class=" img-hover-zoom" style="height:100%" @click="()=>{medium = true; link = 'https://i.pinimg.com/originals/e1/00/1e/e1001e86903d5fccba2a7e83a0547bd4.jpg'}">
+          <q-img width="100%" height="100%" class="rounded-borders img" src="https://i.pinimg.com/originals/e1/00/1e/e1001e86903d5fccba2a7e83a0547bd4.jpg" />
+        </div>
+      </div>
+
+      <div class="col-12 col-md-5 q-pa-lg">
+        <div class=" img-hover-zoom" style="height:100%" @click="()=>{medium = true; link = 'https://i.pinimg.com/originals/33/8a/55/338a55304e6dc72ef9258968ce045e8d.jpg'}">
+          <q-img width="100%" height="100%" class="rounded-borders img" src="https://i.pinimg.com/originals/33/8a/55/338a55304e6dc72ef9258968ce045e8d.jpg" />
+        </div>
+      </div>
+
+      <div class="col-12 col-md-1"></div>
+
+    </div>
+    <!-- Cuarta fila -->
+    <div  class="row text-center" >
+      <div class="col-12 col-md-1"></div>
+
+      <div class="col-12 col-md-5 q-pa-lg">
+        <div class=" img-hover-zoom" style="height:100%" @click="()=>{medium = true; link = 'https://i.pinimg.com/originals/e1/00/1e/e1001e86903d5fccba2a7e83a0547bd4.jpg'}">
+          <q-img width="100%" height="100%" class="rounded-borders img" src="https://i.pinimg.com/originals/e1/00/1e/e1001e86903d5fccba2a7e83a0547bd4.jpg" />
+        </div>
+      </div>
+
+      <div class="col-12 col-md-5 q-pa-lg">
+        <div class=" img-hover-zoom" style="height:100%" @click="()=>{medium = true; link = 'https://fondosmil.com/fondo/2257.jpg'}">
+          <q-img width="100%" height="100%" class="rounded-borders img" src="https://fondosmil.com/fondo/2257.jpg" />
+        </div>
+      </div>
+
+      <div class="col-12 col-md-1"></div>
+    </div>
+
+    <!-- Row para contacto -->
+    <div class="row text-center items-baseline" id="contacto" >
+      <div class="col-12 col-md-1"></div>
+      <div class="col-12 col-md-5 q-pa-lg text-left ">
+          <p class="bg-black q-ma-none text-white"> CONTACTO </p>
+          <p class="bg-black q-ma-none text-white mail"> EMAIL@CONTACTO.COM</p>
+      </div>
+
+      <div class="col-12 col-md-5 q-pa-lg">
+        <q-form v-on:submit="sendMessage" >
+          <div class="img-hover-zoom bg-black text-left">
+            <h5 class="text-white q-my-none">Nombre</h5>
+            <q-input ref="input1" dense square dark color="white" v-model="nombre" outlined  class="bg-black q-mb-xs input" lazy-rules :rules="[val => !!val || 'Ingrese su nombre']"/>
+            <h5 class="text-white q-my-none">Correo electronico</h5>
+            <q-input ref="input2" dense square dark color="white" v-model="correo" outlined class="bg-black q-mb-xs input" lazy-rules :rules="[val => !!val || 'Ingrese un correo']"/>
+            <h5 class="text-white q-my-none">Mensaje</h5>
+            <q-input ref="input3" dense square type="textarea"  dark color="white" v-model="mensaje" outlined class="q-mb-xs input" lazy-rules :rules="[val => !!val || 'Ingrese un mensaje']"/>
+            <q-btn outline rounded color="white" type="submit" class="bg-white text-black q-px-lg" label="Enviar" />
+          </div>
+        </q-form>
+      </div>
+      <div class="col-12 col-md-1"></div>
+    </div>
+    <!-- Dialog fotos -->
+    <q-dialog
+      v-model="medium"
+    >
+      <q-card style="width: 900px; max-width: 1000px;">
+        <q-card-section>
+          <q-img width="100%"  class="rounded-borders img" :src=link />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+        <!-- Dialog video -->
+    <q-dialog
+      v-model="mediumVideo"
+    >
+      <q-card style="width: 900px; max-width: 1000px;">
+        <q-card-section>
+           <iframe width="100%" height="500" :src=linkVideo title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'PageIndex',
+  data: function () {
+    return {
+      mensaje: '',
+      nombre: '',
+      correo: '',
+      medium: false,
+      mediumVideo: false,
+      link: '',
+      linkVideo: ''
+    }
+  },
+  methods: {
+    sendMessage: function () {
+      console.log('MENSAJE ENVIADO')
+      this.reset()
+    },
+    reset: function () {
+      this.nombre = ''
+      this.correo = ''
+      this.mensaje = ''
+      this.$refs.input1.resetValidation()
+      this.$refs.input2.resetValidation()
+      this.$refs.input3.resetValidation()
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+.img-hover-zoom:hover .playBtn
+  color: #00000000
+  transform: scale(1.2)
+  transition: 0.45s all ease-in-out
+
+.videoContainer
+  position: relative
+
+.playBtn
+  position: absolute
+  width: 96px
+  height: 96px
+  left: 50%
+  top: 50%
+  margin-left: -48px /*half of the width */
+  margin-top: -48px /*half of the height */
+  transition: 0.45s all ease-in-out
+
+.img-hover-zoom
+  overflow: hidden
+  margin: 0 auto
+  display:table
+  border-radius: 2px
+
+.img
+  transition: 0.45s all ease-in-out
+  border-radius: 2px
+
+.img-hover-zoom:hover .img
+  transform: scale(1.1)
+
+@import url('https://fonts.googleapis.com/css?family=Black+Han+Sans')
+p
+  font-family: 'Black Han Sans'
+  font-size: 30px
+  font-weight: 100
+
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap')
+h5
+  font-family: 'Lato', sans-serif
+  font-size: 20px
+
+.mail
+  font-size: 28px
+
+#portafolio
+  scroll-margin-top: 5rem
+</style>
